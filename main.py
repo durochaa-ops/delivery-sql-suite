65e7a1db7735590b50436d3587200047b8b9e42a
Eduardo Rocha <durochaa@gmail.com>
2026-08-28 00:20:16 +0200

    Sprint 1: joins, CASE for the team rename, filtered bug counts per team

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ import duckdb
 
 
 query = """
-SELECT ISSUE_ID, FROM_STATUS, TO_STATUS, CHANGED_AT, CHANGED_BY, count(*)
-FROM 'data/transitions.csv'
-GROUP BY ISSUE_ID, FROM_STATUS, TO_STATUS, CHANGED_AT, CHANGED_BY
-HAVING count(*) > 1
+SELECT
+  CASE
+    WHEN team_name = 'Search' THEN 'Discovery'  -- renamed 2026-01-15, same team
+    ELSE team_name
+  END AS team,
+  count(*) AS issue_count
+FROM 'data/issues.csv' AS i
+JOIN 'data/teams.csv' AS t ON i.team_id = t.team_id
+  WHERE issue_type = 'Bug' 
+
+GROUP BY team 
+    
+
+ORDER BY issue_count DESC
 """
 
 result = duckdb.sql(query)
